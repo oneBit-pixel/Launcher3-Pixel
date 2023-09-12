b70505e62ea1ab70b59859f27dbb640f1967750d
zhangxuyang <zhangxuyang@saintvlly.com>
2023-09-12 16:18:26 +0800

    Initial commit

---
+++ src/com/android/launcher3/AppFilter.java
@@ -0,0 +1,18 @@
+package com.android.launcher3;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.UserHandle;
+
+import com.onepixel.launcher3.R;
+
+public class AppFilter {
+
+    public static AppFilter newInstance(Context context) {
+        return Utilities.getOverrideObject(AppFilter.class, context, R.string.app_filter_class);
+    }
+
+    public boolean shouldShowApp(ComponentName app, UserHandle user) {
+        return true;
+    }
+}
